055add978b48c1a35b9e20acfbb2499222e95059
Damien <dasseya1@gmail.com>
2017-10-25 20:15:19 -0400

    Settinp up login functionality -part I

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';   
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends Component {
     state = {
@@ -11,10 +13,27 @@ class LoginForm extends Component {
         errors: {}
     }
 
+    onChange = e => 
+        this.setState({ 
+            data: {...this.state.data, [e.target.name]: e.target.value }
+    });
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+    };
+
+    validate = (data) => {
+        const errors = {};
+        if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+        if (!data.password) errors.password = "Can't be blank!";
+        return errors;
+    }
+
     render() {
         const { data } = this.state;
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label htmlFor="email">Email</label>
                     <input 
@@ -23,6 +42,17 @@ class LoginForm extends Component {
                         name="email" 
                         placeholder="example@example.com"
                         value={data.email}
+                        onChange={this.onChange}
+                        />
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="email">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Make it secure"
+                        value={data.password}
                         onChange={this.onChange}
                         />
                 </Form.Field>
