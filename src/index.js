4e5534d4af4a9e51e2a1c8d47ee967cb0f2af2fc
Damien Asseya <dasseya1@gmail.com>
2017-10-27 19:15:53 +0000

    Installed redux, react-redux, thunk and added store to index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
+
+
+const store = createStore(rootReducer, appplyMiddleware(thunk));
 
 ReactDOM.render(
     <BrowserRouter>
